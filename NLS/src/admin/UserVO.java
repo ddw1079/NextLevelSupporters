c9fda5018357256600e46b32503fe1dcf3e60e51
SongJuUuU <azc117@naver.com>
2025-04-18 14:42:54 +0900

    파일 구조 수정

--- NLS/src/admin/UserVO.java
+++ NLS/src/admin/UserVO.java
@@ -4,8 +4,9 @@ public class UserVO{
 	private int id;
 	private String login_id;
 	private String name;
+	private int user_type;
 	private String phone;
-	private Boolean is_active;
+	private String is_active;
 	
 	public int getId() {
 		return id;
@@ -31,6 +32,14 @@ public class UserVO{
 		this.name = name;
 	}
 
+	public int getUser_type() {
+		return user_type;
+	}
+
+	public void setUser_type(int user_type) {
+		this.user_type = user_type;
+	}
+
 	public String getPhone() {
 		return phone;
 	}
@@ -39,19 +48,20 @@ public class UserVO{
 		this.phone = phone;
 	}
 
-	public Boolean getIs_active() {
+	public String getIs_active() {
 		return is_active;
 	}
 
-	public void setIs_active(Boolean is_active) {
+	public void setIs_active(String is_active) {
 		this.is_active = is_active;
 	}
 
-	public UserVO(int id, String login_id, String name, String phone, Boolean is_active) {
+	public UserVO(int id, String login_id, String name, int user_type, String phone, String is_active) {
 		super();
 		this.id = id;
 		this.login_id = login_id;
 		this.name = name;
+		this.user_type = user_type;
 		this.phone = phone;
 		this.is_active = is_active;
 	}
--- NLS/src/login/LoginDAO.java
+++ NLS/src/login/LoginDAO.java
@@ -7,6 +7,7 @@ import db.ConnectDB;
 import java.util.ArrayList;
 import java.sql.PreparedStatement;
 
+
 public class LoginDAO implements Readable {
 
     private Connection con;
@@ -16,22 +17,8 @@ public class LoginDAO implements Readable {
     @Override
     public ArrayList<BaseVO> read() throws SQLException {
         ArrayList<BaseVO> tiarray = new ArrayList<>();
-        String sql = "SELECT * FROM USER WHERE id= 받아온거";
-        pstmt = con.prepareStatement(sql);
-        rs = pstmt.executeQuery();
-
-        while (rs.next()) {
-            //(2) 종이박스에서 한명씩 꺼내
-            int id = rs.getInt("id");
-            //id:자바의 변수명		//id:db의 컬럼명
-            String name = rs.getString("name");
-            String tel = rs.getString("tel");
-            Date d = rs.getDate("d");
-
-            LoginVO = new LoginVo(id, name, tel, d);
-            //(3) 객체 생성
-
-            tiarray.add(tv);
+       
+        
             //(4) 컬렉션에 넣기
         }
         return tiarray;
@@ -39,9 +26,10 @@ public class LoginDAO implements Readable {
 
     @Override
     public List<BaseVO> read(int target) throws SQLException {
-        List<BaseVO> dummy;
+        String sql = "SELECT * FROM USER WHERE id= 받아온거";
+        pstmt = con.prepareStatement(sql);
+        rs = pstmt.executeQuery();
 
-        return dummy;
 
     }
 }
