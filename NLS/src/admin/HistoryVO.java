17468676c11f6fd910991b02041aefa960c228b4
[sindorim7200] <[park7041501@gmail.com]>
2025-04-21 13:17:23 +0900

    historyVO

--- NLS/src/admin/HistoryVO.java
+++ NLS/src/admin/HistoryVO.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 public class HistoryVO{
 	private int giver_id;
+	private String giver_logid;
 	private int receiver_id;
+	private String receiver_logid;
 	private int amount;
 	private Date date;
 	private String is_received;
@@ -49,10 +51,28 @@ public class HistoryVO{
 		this.is_received = is_received;
 	}
 
-	public HistoryVO(int giver_id, int receiver_id, int amount, Date date, String is_received) {
+	public String getGiver_logid() {
+		return giver_logid;
+	}
+
+	public void setGived_logid(String giver_logid) {
+		this.giver_logid = giver_logid;
+	}
+
+	public String getReceiver_logid() {
+		return receiver_logid;
+	}
+
+	public void setReceiver_logid(String receiver_logid) {
+		this.receiver_logid = receiver_logid;
+	}
+
+	public HistoryVO(int giver_id,String giver_logid, int receiver_id,String receiver_logid, int amount, Date date, String is_received) {
 		super();
 		this.giver_id = giver_id;
+		this.giver_logid = giver_logid;
 		this.receiver_id = receiver_id;
+		this.receiver_logid = receiver_logid;
 		this.amount = amount;
 		this.date = date;
 		this.is_received = is_received;
